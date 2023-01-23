61f1741513abd0e9921103774f0d441a43627e14
prabin <kdlprabin444@gmail.com>
2023-01-23 21:39:17 +0545

    week 10 assignment

---
+++ Week10/src/Database/CreateDatabase.java
@@ -0,0 +1,18 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CreateDatabase {
+    public CreateDatabase(){
+        CreateConnection connect = new CreateConnection();
+        Connection connection = connect.con;
+        try{
+            Statement st = connection.createStatement();
+            st.executeUpdate("CREATE DATABASE IF NOT EXISTS database_name;\n");
+        }catch(SQLException e){
+            System.out.println(e);
+        }
+    }
+}
--- Week10/src/Frontend/Display.java
+++ Week10/src/Frontend/Display.java
@@ -1,18 +1,44 @@
 package Frontend;
 
+import Database.CreateConnection;
+
 import javax.swing.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class Display extends JFrame{
     private JPanel Display;
-    private JButton signUpButton;
-    private JButton signInButton;
-    private JPanel IdData;
-    private JPanel LastNameData;
-    private JPanel FirstNameData;
+    private JList list1;
+    private JList list2;
+    private JList list3;
+    private ArrayList<Integer> idList = new ArrayList<>();
+    private ArrayList<String> firstList = new ArrayList<>();
+    private ArrayList<String> lastList = new ArrayList<>();
 
     public Display(){
+        CreateConnection connect = new CreateConnection();
+        Connection connection = connect.con;
+        try{
+            Statement St = connection.createStatement();
+            ResultSet res = St.executeQuery("Select * from week10");
+            while(res.next()){
+                idList.add(res.getInt("id"));
+                firstList.add(res.getString("First_name"));
+                lastList.add(res.getString("Last_name"));
+            }
+        }catch(SQLException e){
+            System.out.println(e);
+        }
         setContentPane(Display);
-        setSize(500,500);
+        setSize(530,300);
+        list1.setListData(idList.toArray());
+        list2.setListData(firstList.toArray());
+        list3.setListData(lastList.toArray());
         setVisible(true);
     }
 }
